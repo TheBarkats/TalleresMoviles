440f64599923445654615573237ba32b5de54db4
TheBarkats <cristopher.arias01@uceva.edu.co>
2025-09-18 21:25:37 -0500

    Elimino carpetas taller1 y taller2 para unificar proyecto

--- taller1/android/app/src/main/kotlin/com/example/taller1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.taller1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- taller2/android/app/src/main/kotlin/com/example/taller2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.taller2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
